193752b8702546a50895ccc3e8f86effa9bf1432
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 05:01:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,10 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-
   array.sort_by {|word| word.length}
+end
 
+def swap_elements(array)
+  array [1], array[2] = array[2], array[1]
+  array
 end 
