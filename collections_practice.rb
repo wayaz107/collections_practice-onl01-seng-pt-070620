486b49eac30b4859eceea703f424a721cd7a2fcc
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 05:31:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  array [1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all{|word| word[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject{|sum,n| sum + n}
+end
+
+def add_s(array)
+  array.collect do|word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
