0225cd90dc925eb6e0f433e9fc90ff0161ddeb9b
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 05:09:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,9 +20,7 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  new_array = []
   array.each do |word|
-    new_array << word[2] = "$"
-  end
-  new_array
+    word[2] = "$"
+  end  
 end
