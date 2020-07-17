76f6945ec72baca8241d1ad6f832cf782ba37ae7
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 05:13:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,18 +22,10 @@ end
 def kesha_maker(array)
   array.each do |word|
     word[2] = "$"
-  end  
+  end
 end
 
 def find_a(array)
   new_array = []
   new_array << array.start_with?("a")
 end
-
-
-
-
-
-
-
-
